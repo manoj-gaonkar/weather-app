7c05dc68f1dc833a85a3333706719548c712ebc5
manoj-gaonkar <manojgaonkar7648@gmail.com>
2022-09-03 19:45:45 +0530

    fully working

--- weather/urls.py
+++ weather/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home' )
+    path('',views.index, name='index' ),
+    path('home',views.home,name='home')
+
 ]
--- weather/views.py
+++ weather/views.py
@@ -6,20 +6,37 @@ import geocoder
 from geopy.geocoders import Nominatim
 
 
+def index(request):
+    return render(request,'weather/index.html')
+
 
 def home(request):
     g = geocoder.ip('me')
     geolocator = Nominatim(user_agent='http')
     location = geolocator.reverse(g.latlng)
     address = location.raw['address']
-    main = address.get('city')
+    main = address.get('city') 
+    if address.get('city'):
+        main = address.get('city') 
+    elif address.get('state'):
+        main = address.get('state')
 
 
     if request.method == "POST":
         city = request.POST.get('city')
+        print(len(city),main)
+        if(city== ""):
+            new_city=main
+        else:
+            new_city = city
+        print(new_city)
         url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=7d52319e84228356ec16d3b335a712b5&units=metric'
-        r = requests.get(url.format(city)).json()
-        print(r)
+        r = requests.get(url.format(new_city)).json()
+        print(r['cod'])
+        if r['cod'] != 200:
+            print("inside cod")
+            return render(request, 'weather/weather.html',{'messages' : f"No results found"})
+        print('outside cod')
         weather_data = {
             'temp': round(r['main']['temp']),
             'desc': r['weather'][0]['description'],
@@ -28,7 +45,6 @@ def home(request):
         context = {
             'weather_data': weather_data, 
         }
-        print("postrew")
     else:
         context ={
             'weather_data':{
